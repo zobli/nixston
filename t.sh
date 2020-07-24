8a85b2ed1dba1d5979e87607be77b585354b0dbe
Stephan Dev <su@stephan.dev>
2020-07-25 00:30:53 +0200

    auto-Jul-25-00:30:53

--- t.sh
+++ t.sh
@@ -5,6 +5,11 @@ mob=true
 while [ $mob = true ]
 do
     echo its true
-    sleep 30s
+    git add index.html
+    sleep 5s
+    now=`date +"%b-%d-%H:%M:%S"`
+    git commit -m auto-$now
+    sleep 5s
     git push
+    sleep 90s
 done
